ef84dafb589af7b0d0460963e96efa9a024c31bd
AnjoYT <33622757+AnjoYT@users.noreply.github.com>
2021-10-24 17:42:27 +0200

    Ćw1

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,10 @@
 public class Main {
 
     public static void main(String[] args) {
-
-        System.out.println("Witaj świecie!");
-
+        System.out.println("Hello world");
+        System.out.printf("Hello world1 %s Hello world2 %d Hello world3\n", "Hello world4", 1);
+        System.out.print("Hello world1");
+        System.out.print(" Hello world2");
+        System.out.print(" Hello world3");
     }
 }
