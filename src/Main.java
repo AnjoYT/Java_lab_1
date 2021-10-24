5fea9f0716681dd31f030ee4cb9add4426e7bfeb
AnjoYT <33622757+AnjoYT@users.noreply.github.com>
2021-10-24 18:15:35 +0200

    Ćw3

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,31 @@
 public class Main {
 
     public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
 
+<<<<<<< Updated upstream
         System.out.println("Witaj świecie!");
+=======
+        /* ćw.3 */
+
+        /*  zad.1 napisać interfejs który bedzie słuzył do wpisania danych osobowych:
+         *   a) Imię, nazwisko, wiek, nr indeksu,
+         *   dane mają być wprowadzane z klawiatury w konsoli
+         *   b) wyświetlić dane za pomocą println i printf
+         */
+        System.out.print("Prosze podac imie: ");
+        String imie = scan.next();
+        System.out.print("Prosze podac nazwisko: ");
+        String nazwisko = scan.next();
+        System.out.print("Prosze podac wiek: ");
+        int wiek = scan.nextInt();
+        System.out.print("Prosze podac nr indeksu: ");
+        int indeks = scan.nextInt();
+
+
+        /*  zad.2 zadeklarować 2 zmienne typu int i wykonać na nich obliczenia arytmetyczn (+, -, *, /, %)
+         *   wynik wyświetlić w konsoli programu
+         */
 
     }
 }
